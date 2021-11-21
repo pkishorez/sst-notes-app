454e26b7b746464cfd5dcfd407c99bc052bbb387
Kishore <kishore@livecontrol.io>
2021-11-21 13:31:15 +0530

    Add storage stack. Add prettier config.

---
+++ stacks/storage-stack.js
@@ -0,0 +1,23 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class StorageStack extends sst.Stack {
+  table;
+  bucket;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    this.table = new sst.Table(this, "Notes", {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: {
+        partitionKey: "userId",
+        sortKey: "noteId",
+      },
+    });
+
+    this.bucket = new sst.Bucket(this, "uploads");
+  }
+}
